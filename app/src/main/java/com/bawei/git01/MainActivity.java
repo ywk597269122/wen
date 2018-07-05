09d80096ad6e1eec899c0c5e0730d4c39091b026
Yang Wen Kai <1553685468@qq.com>
2018-07-05 20:16:02 +0800

    init

--- app/src/main/java/com/bawei/git01/MainActivity.java
+++ app/src/main/java/com/bawei/git01/MainActivity.java
@@ -1,13 +1,21 @@
 package com.bawei.git01;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*1231321*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
     }
 }
